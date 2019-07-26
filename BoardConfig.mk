7a48fd6ed03f885706bfef3f67ce61bb289f9572
The-DarkBeast <pranay@firehound.org>
2019-07-26 09:27:00 +0200

    garlic: Use garlic_defconfig for kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ DEVICE_PATH := device/yu/garlic
 TARGET_OTA_ASSERT_DEVICE := garlic
 
 # Kernel
-TARGET_KERNEL_CONFIG := garlic-perf_defconfig
+TARGET_KERNEL_CONFIG := garlic_defconfig
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
